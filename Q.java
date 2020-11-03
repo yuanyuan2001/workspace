ad3d87196ed3f9f2b9e3a72a52cb20e5946d93c1
yuanyuan2001 <1075319354@qq.com>
2020-11-03 21:02:54 +0800

    first commit test

---
+++ Q.java
@@ -0,0 +1,37 @@
+package gytuy;
+
+public class Q {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("a"+"b"+"pow(a,b)");
+		int a,b,n,pow;
+		 n=2;
+		 
+		 pow=1;
+		 for(a=1;a<=5;a++)
+		 {
+			 
+			 pow=1;for(b=1;b<=n;b++)
+		 {
+			 
+				 pow*=a;
+		 }
+		
+			 n++;
+		 
+			 b=b-1;
+		 System.out.println(""+a+""+b+""+pow);
+		 }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
